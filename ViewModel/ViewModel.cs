5e4a8845c0398548566a7a22fedba50183e6a286
Вахрамеев Владислав <umperators@yandex.ru>
2020-03-22 15:46:01 +0000

    Вахрамеев Мелкие изменения.

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -11,6 +11,7 @@ using System.Composition.Hosting;
 using System.Reactive.Linq;
 using System.IO;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace ViewModel
 {
@@ -117,7 +118,31 @@ namespace ViewModel
 
             LoadAll = ReactiveCommand.Create<IGraphic, Unit>(_ =>
             {
-                var file = Process.Start("explorer.exe", @"/n,/select," + Directory.GetCurrentDirectory());   
+                var fileContent = string.Empty;
+                var filePath = string.Empty;
+
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Get the path of specified file
+                        filePath = openFileDialog.FileName;
+
+                        //Read the contents of the file into a stream
+                        var fileStream = openFileDialog.OpenFile();
+
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            fileContent = reader.ReadToEnd();
+                        }
+                    }
+                }
+                //var file = Process.Start("explorer.exe", @"/n,/select," + Directory.GetCurrentDirectory());   
                 return default;
             });
         }
