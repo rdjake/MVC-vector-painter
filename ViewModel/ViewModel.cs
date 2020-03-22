3372c7d0c9e536d7968a65b66619a8f9ff8dcabf
rdjake <umperators@yandex.ru>
2020-03-22 13:09:13 +0000

    Вахрамеев Добавил каркас реактивной команды сохранения всех фигур в файл. Сохранять можно много раз, запись происходит каждый раз в отдельный файл.

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -1,4 +1,4 @@
-﻿using ReactiveUI;
+using ReactiveUI;
 using System;
 using DynamicData;
 using MiniEditor;
@@ -9,6 +9,8 @@ using System.Composition;
 using System.Linq;
 using System.Composition.Hosting;
 using System.Reactive.Linq;
+using System.IO;
+
 namespace ViewModel
 {
     public class ViewModel:ReactiveObject
@@ -19,7 +21,7 @@ namespace ViewModel
             public IEnumerable<Lazy<IFigureDescriptor, FigureDescriptorMetadata>> AvailableFigures { get; set; }
         }
         static ImportClass importClass;
-        SourceList<IFigure> Figures=new SourceList<IFigure>();
+        SourceList<IFigure> Figures=new SourceList<IFigure>(); 
         ReadOnlyObservableCollection<IFigure> allFigures;
         public ReadOnlyObservableCollection<IFigure> AllFigures => allFigures;
         public IEnumerable<string> FigureTypes => importClass.AvailableFigures.Select(fig => fig.Metadata.Name);
@@ -32,10 +34,11 @@ namespace ViewModel
         {
             return importClass.AvailableFigures.First(f => f.Metadata.Name == type).Value.Create(points);
         }
-
+        
         public ReactiveCommand<IFigure,Unit> Add { get; }
         public ReactiveCommand<IFigure, Unit> Delete { get; }
         public ReactiveCommand<IGraphic, Unit> Draw { get; }
+        public ReactiveCommand<IGraphic, Unit> SaveAll { get; }
         static ViewModel()
         {
          System.Reflection.Assembly[] assemblies = { typeof(Point).Assembly};
@@ -53,25 +56,53 @@ namespace ViewModel
 
         public ViewModel()
         {
-            Figures.Connect().Bind(out allFigures);
+            Figures.Connect().Bind(out allFigures).Subscribe();
+
             Add = ReactiveCommand.Create<IFigure, Unit>(
-                fig => 
-                {
-                    Figures.Add(fig);
-                    return default;
-                });
+            fig => 
+            {
+                Figures.Add(fig);                 
+                return default;
+            });
+
             Delete = ReactiveCommand.Create<IFigure, Unit>(
-                fig =>
-                {
-                    Figures.Remove(fig);
-                    return default;
-                },Figures.CountChanged.Select(i=>i>0));
+            fig =>
+            {
+                Figures.Remove(fig);
+                return default;
+            },Figures.CountChanged.Select(i=>i>0));
+
             Draw = ReactiveCommand.Create<IGraphic, Unit>(graphic =>
             {
                 foreach (var figure in Figures.Items)
                     figure.Draw(graphic);
                 return default;
             });
+
+            SaveAll = ReactiveCommand.Create<IGraphic, Unit>(_ =>
+            {
+                int i = 0;
+                string path = Directory.GetCurrentDirectory() + @"\AllFigures_" + i + ".txt"; ;
+                while (File.Exists(path)) {
+                    i++;
+                    path = Directory.GetCurrentDirectory() + @"\AllFigures_" + i + ".txt"; 
+                } 
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    foreach (var figure in Figures.Items)
+                    {
+                        sw.WriteLine(figure.Name); //type
+                        sw.WriteLine(figure.Parameters.Count()); //number of parameters
+                        foreach (var p in figure.Parameters) {
+                            sw.WriteLine(p);
+                        }
+
+                    }
+                    sw.Close();
+                }
+                
+                return default;
+            },Figures.CountChanged.Select(i => i > 0));
         }
     }
 }
