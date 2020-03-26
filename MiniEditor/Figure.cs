14f6ee72e96c30c8810ba5f113ef7abb26856339
Вахрамеев Владислав <umperators@yandex.ru>
2020-03-26 11:30:15 +0000

    Вахрамеев. Мелкие изменения

--- MiniEditor/Figure.cs
+++ MiniEditor/Figure.cs
@@ -18,16 +18,16 @@ namespace MiniEditor
     {
         public byte R, G, B, A;
     }
-    //Структура для хранения цвета фигуры
-    public struct FigureColor
+    public struct Brush
     {
-        Color Fill;
-        Color Line;
-        double LineThickness;
+        public Color Fill;
+        public Color Line;
+        public double Thickness;
     }
     public interface IFigure
     {
         string Name { get; }
+
         IEnumerable<string> Parameters { get; }
         object this[string name] { get; set; }
         void Draw(IGraphic graphic);
@@ -36,7 +36,7 @@ namespace MiniEditor
     {
         string Name { get; }
         int NumberOfPoints { get; }
-        IFigure Create(IEnumerable<Point> vertex);
+        IFigure Create(IEnumerable<Point> vertex, Brush brush);
     }
     public class FigureDescriptorMetadata
     {
@@ -46,13 +46,16 @@ namespace MiniEditor
 
     public class Line : IFigure
     {
-        public Line(Point a, Point b)
+        public Line(Point a, Point b, Brush br)
         {
-            A = a; B = b;
+            A = a;
+            B = b;
+            brush = br;
         }
         Point A;
         Point B;
-        public IEnumerable<string> Parameters { get; } = new[] { "A", "B" };
+        Brush brush;
+        public IEnumerable<string> Parameters { get; } = new[] { "A", "B", "brush"};
 
         public object this[string name]
         {
@@ -62,6 +65,7 @@ namespace MiniEditor
                 {
                     case "A": return A;
                     case "B": return B;
+                    case "brush": return brush;
                     default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
                 }
 
@@ -74,6 +78,14 @@ namespace MiniEditor
                     {
                         case "A": A = p; break;
                         case "B": B = p; break;
+                        default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
+                    }
+                }
+                if (value is Brush br) 
+                {
+                    switch (name)
+                    {
+                        case "brush": brush = br; break;
                         default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
                     }
                 }
@@ -119,11 +131,11 @@ namespace MiniEditor
         public string Name => "Line";
         public int NumberOfPoints => 2;
 
-        public IFigure Create(IEnumerable<Point> vertex)
+        public IFigure Create(IEnumerable<Point> vertex, Brush brush)
         {
             var points = vertex.ToArray();
             if (points.Length != 2) throw new ArgumentOutOfRangeException($"Bad number of parameters {points.Length}");
-            return new Line(points[0], points[1]);
+            return new Line(points[0], points[1], brush);
         }
     }
 
@@ -131,17 +143,19 @@ namespace MiniEditor
 
     public class Rectangle : IFigure
     {
-        public Rectangle(Point a, Point c)
+        public Rectangle(Point a, Point c, Brush br)
         {
             A = a; C = c;
             B = new Point { X = A.X, Y = C.Y };
             D = new Point { X = C.X, Y = A.Y };
+            brush = br;
         }
         Point A;
         Point C;
         Point B;
         Point D;
-        public IEnumerable<string> Parameters { get; } = new[] { "A", "C" };
+        Brush brush;
+        public IEnumerable<string> Parameters { get; } = new[] { "A", "C", "brush" };
 
         public object this[string name]
         {
@@ -151,6 +165,7 @@ namespace MiniEditor
                 {
                     case "A": return A;
                     case "C": return C;
+                    case "brush": return brush;
                     default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
                 }
 
@@ -163,6 +178,14 @@ namespace MiniEditor
                     {
                         case "A": A = p; break;
                         case "C": C = p; break;
+                        default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
+                    }
+                }
+                if (value is Brush br)
+                {
+                    switch (name)
+                    {
+                        case "brush": brush = br; break;
                         default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
                     }
                 }
@@ -222,11 +245,11 @@ namespace MiniEditor
     {
         public string Name => "Rectangle";
         public int NumberOfPoints => 2;
-        public IFigure Create(IEnumerable<Point> vertex)
+        public IFigure Create(IEnumerable<Point> vertex, Brush brush)
         {
             var points = vertex.ToArray();
             if (points.Length != 2) throw new ArgumentOutOfRangeException($"Bad number of parameters {points.Length}");
-            return new Rectangle(points[0], points[1]);
+            return new Rectangle(points[0], points[1], brush);
         }
     }
 
@@ -234,17 +257,19 @@ namespace MiniEditor
 
     public class Circle : IFigure
     {
-        public Circle(Point c1, Point c2)
+        public Circle(Point c1, Point c2, Brush br)
         {
             C = c1; 
             C2 = c2;
             R = Math.Sqrt((c2.X - c1.X)* (c2.X - c1.X) + (c2.Y - c1.Y) * (c2.Y - c1.Y));
+            brush = br;
         }
 
         Point C;
         Point C2;
         double R;
-        public IEnumerable<string> Parameters { get; } = new[] { "C", "C2" };
+        Brush brush;
+        public IEnumerable<string> Parameters { get; } = new[] { "C", "C2", "brush" };
 
         public object this[string name]
         {
@@ -254,6 +279,7 @@ namespace MiniEditor
                 {
                     case "C": return C;
                     case "C2": return C2;
+                    case "brush": return brush;
                     default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
                 }
 
@@ -266,6 +292,14 @@ namespace MiniEditor
                     {
                         case "C": C = p; break;
                         case "C2": C2 = p; break;
+                        default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
+                    }
+                }
+                if (value is Brush br)
+                {
+                    switch (name)
+                    {
+                        case "brush": brush = br; break;
                         default: throw new ArgumentOutOfRangeException($"Unknown parameter {name}");
                     }
                 }
@@ -298,11 +332,11 @@ namespace MiniEditor
     {
         public string Name => "Circle";
         public int NumberOfPoints => 2;
-        public IFigure Create(IEnumerable<Point> vertex)
+        public IFigure Create(IEnumerable<Point> vertex, Brush brush)
         {
             var points = vertex.ToArray();
             if (points.Length != 2) throw new ArgumentOutOfRangeException($"Bad number of parameters {points.Length}");
-            return new Circle(points[0], points[1]);
+            return new Circle(points[0], points[1], brush);
         }
     }
 }
